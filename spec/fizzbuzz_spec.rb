dfea944faa08c69c5519a1b0e2135937c517b840
Chloe <chloemontgomery01@gmail.com>
2018-12-19 14:38:23 +0000

    Add test for fizzbuzz(15)

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,12 @@
 require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+end
+
+describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
